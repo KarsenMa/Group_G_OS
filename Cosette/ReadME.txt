698550288e386b85f4f29138b586d9f7ffafa098
cocobyte1 <coco@bytefamily.com>
2025-04-02 20:09:25 -0500

    Updated readme, shared memory framework added

--- Cosette/ReadME.txt
+++ Cosette/ReadME.txt
@@ -1,1 +1,6 @@
+This file is written in C++. It is not usable without being integrated with the other files.
+Please use main.cpp to run the program. 
 
+
+this file requires that the compilation statement include 
+gcc <name>.cpp -pthread -lrt 
---
+++ Cosette/shared_Mem.cpp
@@ -0,0 +1,41 @@
+/*  Group G
+    Author Name: Cosette Byte
+    Email: cosette.byte@okstate.edu
+    Date: 3/31/2025
+    Program Description: This file contains methods to create a shared memory to be used to hold
+    mutex and semaphore states.
+*/
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+
+void mem_setup(){
+    const char *name = "";
+    int oflag = O_CREAT | ORDWR; // creates memory object, set to read and write
+    mode_t mode  = ; // sets directory permissions of object.
+
+    unsigned int length = 4096; // size of memory object in bytes
+
+    void* mem_ptr; // pointer to memory object
+
+
+    int shm_fd = shm_open(name, oflag, mode);
+    
+    ftruncate(shm_fd, length);
+
+    mem_ptr = *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off);
+
+
+
+}
+
+void mem_close(int fd, unsigned int length, const char* name){
+    // unallocated the memory 
+    munmap(fd, length); //unmap the memory
+
+    shm_unlink(name); //unlink the shared memory
+
+}
+
