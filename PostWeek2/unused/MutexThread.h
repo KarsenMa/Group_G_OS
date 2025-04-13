5ddd219f92ddf72dc747190d8f568e4d58d58ea8
DamianSilvar <163705608+DamianSilvar@users.noreply.github.com>
2025-04-13 18:47:19 -0500

    Update MutexThread.h

--- PostWeek2/unused/MutexThread.h
+++ PostWeek2/unused/MutexThread.h
@@ -1,11 +1,34 @@
-// Group G
-//Author: Damian Silvar
-//Email: damian.silvar@okstate.edu
-//Date: 4-11-25
+/*
+Group: G
+Author: Damian Silvar
+Email: damian.silvar@okstate.edu
+Date: 04-11-2025
+*/
 
 #ifndef MUTEXTHREAD_H
 #define MUTEXTHREAD_H
 
-void startThreads();
+#include <semaphore.h>   // For POSIX semaphore functions
+#include <string>        // For std::string
+
+// Class to manage a named semaphore as a mutex
+class MutexThread {
+private:
+    sem_t* mutex;        // Pointer to semaphore (mutex)
+    std::string name;    // Name of semaphore
+
+public:
+    // Creates or opens a named semaphore
+    MutexThread(const std::string& mutexName);
+
+    // Closes and unlinks the semaphore
+    ~MutexThread();
+
+    // Locks, waits, the semaphore
+    void lock();
+
+    // Unlocks, posts, the semaphore
+    void unlock();
+};
 
 #endif
