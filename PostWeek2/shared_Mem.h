dc8d25ee45e55ca92a365a0e38c0b1e79dc74635
KarsenMa <112121370+KarsenMa@users.noreply.github.com>
2025-04-13 12:18:02 -0500

    Update shared_Mem.h for Resource Allocation Table

--- PostWeek2/shared_Mem.h
+++ PostWeek2/shared_Mem.h
@@ -16,14 +16,13 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-typedef struct{ 
-
-}
 
 typedef struct {
-  int num_mutex;
-  int num_sem;
-  int sem_values[];
+    int num_mutex;
+    int num_sem;
+    int num_trains;
+    int num_intersections;
+    pthread_mutex_t rat_mutex;
 } shared_mem_t;
 
 
@@ -31,7 +30,7 @@ typedef struct {
 class shared_Mem { 
 public:
     const char *name = "sharedMemory"; // Name for the shared memory object
-    void* mem_setup(int num_mutex, int num_sem,  const int sem_values[]);
+    void* mem_setup(int num_mutex, int num_sem,  const int sem_values[], int num_trains, int num_intersections);
     void mem_close(void* ptr);
 };
 
